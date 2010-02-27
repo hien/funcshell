eff2d09c695eef21d6b6f8c7e8e999ac63b00654
Silas Sewell <silas@sewell.ch>
2010-02-27 08:47:52 -0700

    Add online and offline test commands

--- funcshell/modules/test.py
+++ funcshell/modules/test.py
@@ -10,10 +10,16 @@ class Test(BaseModule):
                 ping=cly.Node(help='Ping clients')(
                     cly.Action(help='Ping clients', callback=self.ping),
                 ),
+                offline=cly.Node(help='Show offline clients')(
+                    cly.Action(help='Show offline clients', callback=self.offline),
+                ),
+                online=cly.Node(help='Show online clients')(
+                    cly.Action(help='Show online clients', callback=self.online),
+                ),
             ),
         )
 
-    def ping(self):
+    def ping(self, show_yes=True, show_no=True):
         try:
             result_list = self.wrap(self.client.overlord().test.ping())
             header = ['Client', 'Available']
@@ -23,9 +29,20 @@ class Test(BaseModule):
                     content.append([host, 'yes'])
                 else:
                     content.append([host, 'no'])
-            self.tabularize(header, content)
+            if show_yes and show_no:
+                self.tabularize(header, content)
+            else:
+                for host, result in content:
+                    if (show_yes and result == 'yes') or (show_no and result == 'no'):
+                        print host
         except Func_Client_Exception, e:
             print 'Func exception: %s' % e
+
+    def offline(self):
+        self.ping(show_yes=False)
+
+    def online(self):
+        self.ping(show_no=False)
 
 def register(shell):
     shell.test = Test(shell.client, shell.grammar)
